03b699e83dc828956d6dd6316666f1efb7ac612e
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-02-18 13:34:52 +0100

    add project

---
+++ src/main/java/StreamsMain.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class StreamsMain {
+    public static void main(String[] args) {
+        final String path = args[0];
+        final CountryCurrencyDataReader dataReader = new CountryCurrencyDataReader();
+        final List<CountryCurrencyData> dataList = dataReader.readDataFromFile(path);
+        dataList.forEach(countryCurrencyData ->
+                System.out.println(countryCurrencyData.getCountry() + " " +
+                        countryCurrencyData.getCurrencyFullName() + " " +
+                        countryCurrencyData.getCurrencyShortName() + " " +
+                        countryCurrencyData.getAmount()));
+
+        final String outputPath = args[1];
+        final CountryCurrencyDataWriter dataWriter = new CountryCurrencyDataWriter();
+        dataWriter.saveDataToFile(outputPath, dataList);
+    }
+}
